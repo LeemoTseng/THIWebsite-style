a66b7b26cbd0e5d2cc082a64e8096b3d2cd03404
LeemoTseng-Mac <leemotseng@LeemoTseng-MacdeiMac.local>
2024-07-01 16:39:30 +0800

    add modal

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
 // date picker
 
-  $( function() {
-    $( ".datepicker" ).datepicker();
-    $( "#anim" ).on( "change", function() {
-      $( ".datepicker" ).datepicker( "option", "showAnim", $( this ).val() );
-    });
-  } );
-
+$(function () {
+  $(".datepicker").datepicker();
+  $("#anim").on("change", function () {
+    $(".datepicker").datepicker("option", "showAnim", $(this).val());
+  });
+});
 
 //隱藏顯示
 
+document.addEventListener("DOMContentLoaded", function () {
+  const colleagueInput = document.querySelector("#colleague");
+  const familyInput = document.querySelector("#family");
+  const colleagueNameDiv = document.querySelector("#colleagueName");
+  const addFamilyBtn = document.querySelector("#addFamilyBtn");
 
-document.addEventListener('DOMContentLoaded', function () {
-    const colleagueInput = document.querySelector('#colleague');
-    const familyInput = document.querySelector('#family');
-    const colleagueNameDiv = document.querySelector('#colleagueName');
-    const addFamilyBtn = document.querySelector('#addFamilyBtn');
-
-    // Function to update the visibility of the colleague name input and add family button
-    function updateVisibility() {
-        if (colleagueInput.checked) {
-            colleagueNameDiv.style.display = 'block';
-            addFamilyBtn.style.display = 'none';
-        } else if (familyInput.checked) {
-            colleagueNameDiv.style.display = 'none';
-            addFamilyBtn.style.display = 'block';
-        }
+  // Function to update the visibility of the colleague name input and add family button
+  function updateVisibility() {
+    if (colleagueInput.checked) {
+      colleagueNameDiv.style.display = "block";
+      addFamilyBtn.style.display = "none";
+    } else if (familyInput.checked) {
+      colleagueNameDiv.style.display = "none";
+      addFamilyBtn.style.display = "block";
     }
+  }
 
-    // Initial check on page load
-    updateVisibility();
+  // Initial check on page load
+  updateVisibility();
 
-    // Add event listeners to radio buttons
-    colleagueInput.addEventListener('change', updateVisibility);
-    familyInput.addEventListener('change', updateVisibility);
+  // Add event listeners to radio buttons
+  colleagueInput.addEventListener("change", updateVisibility);
+  familyInput.addEventListener("change", updateVisibility);
+});
+
+//tooltip
+
+document.addEventListener("DOMContentLoaded", function () {
+  const tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
 });
